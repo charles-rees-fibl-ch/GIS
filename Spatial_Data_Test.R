3a991f8feb38f2362a494ac73b4bda9d6c62469e
charles-rees-fibl-ch <138202644+charles-rees-fibl-ch@users.noreply.github.com>
2023-07-03 13:13:49 +0200

    Updates and additional functions

--- Spatial_Data_Test.R
+++ Spatial_Data_Test.R
@@ -3,6 +3,8 @@
 library(tidyverse)
 library(MODISTools)
 library(raster)
+library(stringr)
+library(lubridate)
 
 ### Extracting Products from MODIS and setting mid point
 
@@ -19,19 +21,63 @@ df_ndvi <- mt_subset(product = "MOD13Q1",               # the chosen product
                      lat = latit,                       # desired lat/lon
                      lon = longi,
                      band = "250m_16_days_NDVI",        # chosen band defining spatial and temporal scale
-                     start = "2022-06-10",              # start date: 10th June 2022
+                     start = "2000-02-18",              # start date: 10th June 2022
                      end = "2023-06-10",                # end date: 10th June 2022
-                     km_lr = 10,                        # kilometers left & right of the chosen location (lat/lon above)
-                     km_ab = 10,                        # kilometers above and below the location
+                     km_lr = 15,                        # kilometers left & right of the chosen location (lat/lon above)
+                     km_ab = 15,                        # kilometers above and below the location
                      site_name = "Abtwil (AG)",         # the site name we want to give the data
                      internal = TRUE,
                      progress = FALSE
 ) %>% 
   as_tibble()
 
-head(df_ndvi)
-
-raster_ndvi <- mt_to_raster(df = df_ndvi) # converting subset into raster
+raster_ndvi <- mt_to_terra(df = df_ndvi) # converting subset into raster
 
 ### Plotting Data
-plot(raster_ndvi[[1:4]], zlim=c(-0.1, 0.95))
+plot(raster_ndvi[[21:24]], zlim=c(-0.1, 0.95))
+
+### 
+scale_factor <- bands %>% 
+  filter(band == "250m_16_days_NDVI") %>% 
+  pull(scale_factor) %>% 
+  as.numeric()
+
+df_ndvi_spatialmean <- df_ndvi %>% 
+  mutate(calendar_date = ymd(calendar_date)) %>%  # make the dates into comprehensible values not X.yyyy.mm.dd
+  group_by(calendar_date) %>%                     # group the data by day 
+  summarise(mean = mean(value), min = min(value), max = max(value)) %>%   # calculate mean, min and max across pixels
+  mutate(mean = mean * scale_factor, min = min * scale_factor, max = max * scale_factor)  # apply scale_factor (see )
+
+df_ndvi_spatialmean %>% 
+  ggplot(aes(x = calendar_date)) +
+  geom_ribbon(aes(ymin = min, ymax = max), fill = "grey70") +
+  geom_line(aes(y = mean))
+
+c
+#------------------------------------------------------------------------------#
+### Pulling LAI
+
+latit <- 47.176508
+longi <- 8.354773
+
+bands <- mt_bands(product = "MOD11A2") %>% as_tibble() # weather at 1 km resolution
+dates <- mt_dates(product = "MOD11A2", lat = latit, lon = longi) %>% as_tibble() # Setting center to home 
+
+df_met <- mt_subset(product = "MOD11A2",               # the chosen product
+                     lat = latit,                       # desired lat/lon
+                     lon = longi,
+                     band = "LST_Day_1km",        # chosen band defining spatial and temporal scale
+                     start = "2023-04-07",              # start date: x
+                     end = "2023-06-18",                # end date: y
+                     km_lr = 25,                        # kilometers left & right of the chosen location (lat/lon above)
+                     km_ab = 25,                        # kilometers above and below the location
+                     site_name = "Abtwil (AG)",         # the site name we want to give the data
+                     internal = TRUE,
+                     progress = FALSE
+) %>% 
+as_tibble()
+
+raster_met <- mt_to_terra(df = df_met) # converting subset into raster
+
+### Plotting Data
+plot(raster_met[[7:12]], zlim=c(285, 315))
