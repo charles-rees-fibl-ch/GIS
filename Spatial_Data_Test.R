846956f5c08688072c5195cc2d4552f0a844985f
charles-rees-fibl-ch <138202644+charles-rees-fibl-ch@users.noreply.github.com>
2023-07-04 13:31:17 +0200

    Basis for combining layers

--- Spatial_Data_Test.R
+++ Spatial_Data_Test.R
@@ -5,6 +5,9 @@ library(MODISTools)
 library(raster)
 library(stringr)
 library(lubridate)
+library(plyr)
+library(sf)
+library(tmap)
 
 ### Extracting Products from MODIS and setting mid point
 
@@ -21,7 +24,7 @@ df_ndvi <- mt_subset(product = "MOD13Q1",               # the chosen product
                      lat = latit,                       # desired lat/lon
                      lon = longi,
                      band = "250m_16_days_NDVI",        # chosen band defining spatial and temporal scale
-                     start = "2000-02-18",              # start date: 10th June 2022
+                     start = "2023-05-25",              # start date: 10th June 2022
                      end = "2023-06-10",                # end date: 10th June 2022
                      km_lr = 15,                        # kilometers left & right of the chosen location (lat/lon above)
                      km_ab = 15,                        # kilometers above and below the location
@@ -34,8 +37,12 @@ df_ndvi <- mt_subset(product = "MOD13Q1",               # the chosen product
 raster_ndvi <- mt_to_terra(df = df_ndvi) # converting subset into raster
 
 ### Plotting Data
-plot(raster_ndvi[[21:24]], zlim=c(-0.1, 0.95))
+plot(raster_ndvi[[1]], zlim=c(-0.1, 0.95))
 
+################################################################################
+switch_on <- 0
+
+if(switch_on == 1){
 ### 
 scale_factor <- bands %>% 
   filter(band == "250m_16_days_NDVI") %>% 
@@ -52,32 +59,15 @@ df_ndvi_spatialmean %>%
   ggplot(aes(x = calendar_date)) +
   geom_ribbon(aes(ymin = min, ymax = max), fill = "grey70") +
   geom_line(aes(y = mean))
+}
+################################################################################
 
-c
-#------------------------------------------------------------------------------#
-### Pulling LAI
+### Gemeinde Grenzen
+Gemeidegrenzen = read_sf("//fibl.ch/FILES/Dep_FSS/2_Projects/2020_CH-SNF_LEAF_35188/02_WorkPackages/WP2_ModelDevelopment/01_Data/00_Public/CH_Gemeindegrenzen/ch.swisstopo.swissboundaries3d-gemeinde-flaeche.fill/swissBOUNDARIES3D_1_3_TLM_HOHEITSGEBIET.shp")
 
-latit <- 47.176508
-longi <- 8.354773
+tm_shape(Gemeidegrenzen) + 
+  tm_shape(raster_ndvi) +
+  tm_polygons() +
+  tm_grid()
 
-bands <- mt_bands(product = "MOD11A2") %>% as_tibble() # weather at 1 km resolution
-dates <- mt_dates(product = "MOD11A2", lat = latit, lon = longi) %>% as_tibble() # Setting center to home 
 
-df_met <- mt_subset(product = "MOD11A2",               # the chosen product
-                     lat = latit,                       # desired lat/lon
-                     lon = longi,
-                     band = "LST_Day_1km",        # chosen band defining spatial and temporal scale
-                     start = "2023-04-07",              # start date: x
-                     end = "2023-06-18",                # end date: y
-                     km_lr = 25,                        # kilometers left & right of the chosen location (lat/lon above)
-                     km_ab = 25,                        # kilometers above and below the location
-                     site_name = "Abtwil (AG)",         # the site name we want to give the data
-                     internal = TRUE,
-                     progress = FALSE
-) %>% 
-as_tibble()
-
-raster_met <- mt_to_terra(df = df_met) # converting subset into raster
-
-### Plotting Data
-plot(raster_met[[7:12]], zlim=c(285, 315))
